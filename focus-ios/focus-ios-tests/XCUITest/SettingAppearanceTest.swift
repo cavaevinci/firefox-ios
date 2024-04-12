55989d63fc95ef26925657c32c98c48d58c8aacc
cavaevinci <ivanevacicweb+private@gmail.com>
2024-04-12 17:22:03 +0200

    Update SettingsAppearanceTest

--- focus-ios/focus-ios-tests/XCUITest/SettingAppearanceTest.swift
+++ focus-ios/focus-ios-tests/XCUITest/SettingAppearanceTest.swift
@@ -5,11 +5,11 @@
 import XCTest
 
 class SettingAppearanceTest: BaseTestCase {
-    let iOS_Settings = XCUIApplication(bundleIdentifier: "com.apple.Preferences")
+    let iOSSettings = XCUIApplication(bundleIdentifier: "com.apple.Preferences")
 
     // Smoketest
     // Check for the basic appearance of the Settings Menu
-    func testCheckSetting() {
+    func navigateToSettings() {
         dismissURLBarFocused()
 
         // Navigate to Settings
@@ -19,13 +19,15 @@ class SettingAppearanceTest: BaseTestCase {
         let settingsButton = app.settingsButton
         waitForExistence(settingsButton, timeout: 10)
         settingsButton.tap()
-
+    }
+    func checkAboutPage() {
         // Check About page
         app.tables.firstMatch.swipeUp()
         let aboutCell = app.cells["settingsViewController.about"]
         waitForExistence(aboutCell, timeout: 10)
         aboutCell.tap()
-
+    }
+    func checkHelpPage() {
         let tablesQuery = app.tables
 
         // Check Help page, wait until the webpage is shown
@@ -39,12 +41,13 @@ class SettingAppearanceTest: BaseTestCase {
 
         // Go back to Settings
         app.navigationBars.buttons.element(boundBy: 0).tap()
-
+    }
+    func checkInformationPage() {
         // Check the initial state of the switch values
         let safariSwitch = app.tables.switches["Safari"]
         waitForExistence(app.tables.switches["Safari"], timeout: 5)
 
-        XCTAssertEqual(safariSwitch.value as! String, "0")
+        XCTAssertEqual(safariSwitch.value as? String, "0")
         safariSwitch.tap()
 
         // Check the information page
@@ -61,38 +64,39 @@ class SettingAppearanceTest: BaseTestCase {
             app.navigationBars.buttons.element(boundBy: 0).tap()
         }
 
-        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockFonts"].value as! String, "0")
+        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockFonts"].value as? String, "0")
         if app.label == "Firefox Focus" {
-            XCTAssertEqual(app.tables.switches["BlockerToggle.SendAnonymousUsageData"].value as! String, "1")
+            XCTAssertEqual(app.tables.switches["BlockerToggle.SendAnonymousUsageData"].value as? String, "1")
         } else {
-            XCTAssertEqual(app.tables.switches["BlockerToggle.SendAnonymousUsageData"].value as! String, "0")
+            XCTAssertEqual(app.tables.switches["BlockerToggle.SendAnonymousUsageData"].value as? String, "0")
         }
-
+    }
+    func checkTrackingProtectionSettingsPage() {
         // Check Tracking Protection Settings page
         app.tables.firstMatch.swipeDown()
         let trackingProtectionCell = app.cells["settingsViewController.trackingCell"]
         waitForHittable(trackingProtectionCell)
         trackingProtectionCell.tap()
 
-        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockAds"].value as! String, "1")
-        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockAnalytics"].value as! String, "1")
-        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockSocial"].value as! String, "1")
+        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockAds"].value as? String, "1")
+        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockAnalytics"].value as? String, "1")
+        XCTAssertEqual(app.tables.switches["BlockerToggle.BlockSocial"].value as? String, "1")
         let otherContentSwitch = app.tables.switches["BlockerToggle.BlockOther"]
         waitForExistence(app.tables.switches["BlockerToggle.BlockOther"], timeout: 5)
-        XCTAssertEqual(otherContentSwitch.value as! String, "0")
+        XCTAssertEqual(otherContentSwitch.value as? String, "0")
 
         otherContentSwitch.tap()
         let alertsQuery = app.alerts
         waitForExistence(alertsQuery.buttons[UIConstants.strings.settingsBlockOtherYes])
         // Say yes this time, the switch should be enabled
         alertsQuery.buttons[UIConstants.strings.settingsBlockOtherYes].tap()
-        XCTAssertEqual(otherContentSwitch.value as! String, "1")
+        XCTAssertEqual(otherContentSwitch.value as? String, "1")
         otherContentSwitch.tap()
 
         // Say No this time, the switch should remain disabled
         otherContentSwitch.tap()
         alertsQuery.buttons[UIConstants.strings.settingsBlockOtherNo].tap()
-        XCTAssertEqual(otherContentSwitch.value as! String, "0")
+        XCTAssertEqual(otherContentSwitch.value as? String, "0")
 
         // Go back to settings
         app.navigationBars.buttons.element(boundBy: 0).tap()
@@ -115,6 +119,13 @@ class SettingAppearanceTest: BaseTestCase {
         app.activate()
         waitForExistence(app.navigationBars["Settings"], timeout: 10)*/
     }
+    func testCheckSetting() {
+        navigateToSettings()
+        checkAboutPage()
+        checkHelpPage()
+        checkInformationPage()
+        checkTrackingProtectionSettingsPage()
+    }
 
     func testOpenInSafari() {
         let safariapp = XCUIApplication(privateWithPath: nil, bundleID: "com.apple.mobilesafari")!
@@ -134,7 +145,8 @@ class SettingAppearanceTest: BaseTestCase {
 
         // Go back to Focus
         // Commenting this part out since this issue is common when coming back to the app
-        // Failed to launch org.mozilla.ios.Focus: The operation couldn’t be completed. Application cannot be launched because it has outstanding termination assertions.
+        // Failed to launch org.mozilla.ios.Focus: The operation couldn’t be completed.
+        // Application cannot be launched because it has outstanding termination assertions.
         // app.activate()
 
         // Now back to Focus
@@ -161,11 +173,11 @@ class SettingAppearanceTest: BaseTestCase {
         // Verify that autocomplete is enabled
         waitForExistence(app.tables.switches["toggleAutocompleteSwitch"])
         let toggle = app.tables.switches["toggleAutocompleteSwitch"]
-        XCTAssertEqual(toggle.value as! String, "1")
+        XCTAssertEqual(toggle.value as? String, "1")
 
         // Turn autocomplete off
         toggle.tap()
-        XCTAssertEqual(toggle.value as! String, "0")
+        XCTAssertEqual(toggle.value as? String, "0")
 
         // Turn autocomplete back on
         toggle.tap()
@@ -241,20 +253,20 @@ class SettingAppearanceTest: BaseTestCase {
         // Check that Safari toggle is off, swipe to get to Safari Integration menu
         waitForExistence(app.otherElements["SIRI SHORTCUTS"], timeout: 10)
         app.otherElements["SIRI SHORTCUTS"].swipeUp()
-        XCTAssertEqual(app.switches["BlockerToggle.Safari"].value! as! String, "0")
+        XCTAssertEqual(app.switches["BlockerToggle.Safari"].value! as? String, "0")
 
-        iOS_Settings.activate()
-        waitForExistence(iOS_Settings.cells["Safari"], timeout: 10)
-        iOS_Settings.cells["Safari"].tap()
-        iOS_Settings.cells["AutoFill"].swipeUp()
+        iOSSettings.activate()
+        waitForExistence(iOSSettings.cells["Safari"], timeout: 10)
+        iOSSettings.cells["Safari"].tap()
+        iOSSettings.cells["AutoFill"].swipeUp()
         if #available(iOS 15.0, *) {
-            iOS_Settings.cells.staticTexts["Extensions"].tap()
+            iOSSettings.cells.staticTexts["Extensions"].tap()
         } else {
-            iOS_Settings.cells.staticTexts["CONTENT_BLOCKERS"].tap()
+            iOSSettings.cells.staticTexts["CONTENT_BLOCKERS"].tap()
         }
-        iOS_Settings.tables.cells.staticTexts["Firefox Focus"].tap()
-        iOS_Settings.tables.cells.switches.element(boundBy: 0).tap()
-        iOS_Settings.terminate()
+        iOSSettings.tables.cells.staticTexts["Firefox Focus"].tap()
+        iOSSettings.tables.cells.switches.element(boundBy: 0).tap()
+        iOSSettings.terminate()
 
         XCUIDevice.shared.press(.home)
         // Let's be sure the app is backgrounded
@@ -265,6 +277,6 @@ class SettingAppearanceTest: BaseTestCase {
         // Go back to the app to verify that the toggle has changed its value
         app.activate()
         waitForExistence(app.navigationBars["Settings"], timeout: 15)
-        XCTAssertEqual(app.switches["BlockerToggle.Safari"].value! as! String, "1")
+        XCTAssertEqual(app.switches["BlockerToggle.Safari"].value! as? String, "1")
     }
 }
